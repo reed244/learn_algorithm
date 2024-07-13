13af913363caf00ed6e4688c78a748f1f2f2ed3b
silencer <2158493577@qq.com>
2024-07-13 21:53:03 +0800

    简单题

---
+++ p1421.cpp
@@ -0,0 +1,12 @@
+//Âå¹ÈP1421 Ğ¡ÓñÂòÎÄ¾ß
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int money=a*10+b;
+    int num=money/19;
+    cout<<num;
+    return 0;
+}
